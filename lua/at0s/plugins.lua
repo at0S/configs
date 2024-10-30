16a7669c380aeab117b63929a23ff17c0ae722d3
Taras Yermolenko <t.yermolenko@gmail.com>
2024-10-30 23:28:50 +1100

    [CONFIG] fix: and now the plugins linked to plugins directory

--- lua/at0s/plugins.lua
+++ lua/at0s/plugins.lua
@@ -14,23 +14,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
 require("lazy").setup("plugins")
-   -- { "williamboman/mason-lspconfig.nvim", name = "mason-lspconfig" },
-   -- { "williamboman/mason.nvim", name = "mason" }, 
-   -- { "neovim/nvim-lspconfig", name = "lspconfig" },
-
--- Configure any other settings here. See the documentation for more details. colorscheme that will be used when installing plugins. install = { colorscheme = { "rose-pine" } }, automatically check for plugin updates checker = { enabled = true }, })
---
---require("mason").setup({
---  ui = {
---    icons = {
---      package_installed = "✓",
---      package_pending = "➜",
---      package_uninstalled = "✗"
---    }
---  }
-})
---
---require("mason-lspconfig").setup()
-
