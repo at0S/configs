e7f73fa3d825b5cd492bf535e75958b3fc58a633
Taras Yermolenko <t.yermolenko@gmail.com>
2024-10-30 21:39:00 +1100

    [CONFIG] new: more telescope mappings

--- lua/at0s/telescope.lua
+++ lua/at0s/telescope.lua
@@ -1,3 +1,6 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find files with a telescope' })
+vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Live grep with telescope' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List buffers with telescope' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'List help tags with telescope' })
