b5f883df060c2dfb81ecb203d2eaee5694ae960e
Taras Yermolenko <t.yermolenko@gmail.com>
2024-10-30 23:31:22 +1100

    [CONFIG] new: and keymaps + config for the tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter", name = "treesitter"
+  "nvim-treesitter/nvim-treesitter", name = "treesitter",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup {
+      run = ":TSUpdate",
+      ensure_installed = "all",
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {"just", "systemverilog"},
+      highlight = {
+        enable = true,
+        disable = {""},
+        additional_vim_regex_highlighting = false,
+      },
+      ident = { enable = true, disable = {"yaml"} }
+    }
+  end
 }
