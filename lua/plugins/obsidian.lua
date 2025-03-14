aa747aa24d0f541d936acaee078eb7f7baf407c4
Taras Yermolenko <t.yermolenko@gmail.com>
2025-03-14 11:54:34 +1100

    [VIM] experimental: adding obsidian plugin

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,21 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "/Users/at0s/Library/Mobile Documents/iCloud~md~obsidian/Documents/my-notes",
+      },
+      {
+        name = "work",
+        path = "/Users/at0s/Library/Mobile Documents/iCloud~md~obsidian/Documents/work",
+      },
+    },
+  },
+}
