cd5728bcfe5dd295c8265f5582af2d0ba8d54df1
Taras Yermolenko <t.yermolenko@gmail.com>
2025-06-07 15:16:20 +1000

    telescope: new keymap, perhaps temporarily
    As of now, <leader>t means Telescope and followed by picker, like g for live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,8 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find files with a telescope' })
-    vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Live grep with telescope' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List buffers with telescope' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'List help tags with telescope' })
+    vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Live grep with telescope' })
+    vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'List buffers with telescope' })
+    vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'List help tags with telescope' })
   end
 }
