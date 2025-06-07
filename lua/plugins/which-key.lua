c8afbf7940bfe3dab855b17c49ca0aaeba7743c8
Taras Yermolenko <t.yermolenko@gmail.com>
2025-06-07 17:07:43 +1000

    which-key: added maps hints, but don't manage bindings here

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,11 +4,21 @@ return {
   opts = {},
   keys = {
     {
-      "<leader>?",
+      "<C-w,k>",
       function()
         require("which-key").show({ global = false })
       end,
       desc = "Buffer local keymaps (which-key)",
     },
   },
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>tg", "<cmd>Telescope live grep<cr>", desc = "Live grep with telescope" },
+      { "<leader>tb", "<cmd>Telescope buffers<cr>", desc = "Find in buffers with telescope" },
+      { "<leader>th", "<cmd>Telescope help_tags<cr>", desc = "List help tags with telescope" },
+      { "<leader>tf", "<cmd>Telescope find_files<cr>", desc = "Find files with telescope" },
+      { "<leader>tf", "<cmd>Telescope find_files<cr>", desc = "Find in commits with telescope" },
+    })
+   end,
 }
