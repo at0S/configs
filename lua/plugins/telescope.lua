43925537f9d6347ed81c6ce8a8dab9fc132b5c56
Taras Yermolenko <t.yermolenko@gmail.com>
2025-06-07 17:09:00 +1000

    telescope: added a git_commits picker and remapped keys once again. Well unless I regularly start using them...

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,13 @@
 return {
-  "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find files with a telescope' })
+    vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Find files with a telescope' })
     vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Live grep with telescope' })
     vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'List buffers with telescope' })
     vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'List help tags with telescope' })
+    vim.keymap.set('n', '<leader>tc', builtin.git_commits, { desc = 'Find in commits with telescope' })
   end
 }
