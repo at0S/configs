776cf41e41976bb9d08c08f23b9fc53a78197e8b
Taras Yermolenko <t.yermolenko@gmail.com>
2025-09-29 10:19:46 +1000

    new: using vim.lsp now directly

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,30 +9,35 @@ return {
     "williamboman/nvim-lspconfig",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "ts_ls", "pyright", "gh_actions_ls", "rust_analyzer"},
+        ensure_installed = {"lua_ls", "ts_ls", "pyright", "gh_actions_ls", "rust_analyzer", "dockerfile_language_server" },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.gh_actions_ls.setup({})
-      lspconfig.rust_analyzer.setup({
-        settings = {
-          ["rust_analyzer"] = {
-            cargo = {
+      vim.lsp.config("dockerfile_language_server", {})
+      vim.lsp.config("gh_actions_ls", {})
+      vim.lsp.config("rust_analyzer", {
+          settings = {
+            ["rust_analyzer"] = {
+              cargo = {
               allFeatures = true,
-            },
+              },
             checkOnSave = {
               command = "clippy"
+              },
             },
           },
-        },
-      })
+        })
+
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("gh_actions_ls")
+      vim.lsp.enable("rust_analyzer")
+      vim.lsp.enable("dockerfile_language_server")
+      
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
